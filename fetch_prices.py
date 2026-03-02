f6a20df4cc83e9ff056c04af1f26dbc766fa8d1f
Aran Phul <aranphul@gmail.com>
2026-03-02 08:58:02 +0000

    chore: written the TODO'S for the fetch prices script

--- fetch_prices.py
+++ fetch_prices.py
@@ -14,6 +14,31 @@ if not api_key:
 
 print("KEY FOUND" if api_key else "KEY MISSING")
 #print(api_key)
-print("Hello Proect")
+
+base_url = "https://financialmodelingprep.com/stable"
+endpoint = "/historical-price-eod/full"
+symbol = "AAPL"
+
+# TODO: build full URL (base_url + endpoint)
+# url = ...
+
+# TODO: build params dict with symbol + apikey
+# params = ...
+
+# TODO: make request (include a timeout)
+# r = requests.get(url, params=params, timeout=30)
+
+# TODO: print status code
+# print(...)
+
+# TODO: raise if bad response (so you see errors clearly)
+# r.raise_for_status()
+
+# TODO: parse JSON and print the “shape”
+# data = r.json()
+# print(type(data))
+
+# HINT: if it's a dict, print the keys:
+# print(data.keys())
 
 
